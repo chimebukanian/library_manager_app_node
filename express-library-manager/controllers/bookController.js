5c90fc1d4c182d1029a4d1cf68ddd11caf789510
chimebukanian <chimebukaanyanwu@gmail.com>
2023-08-14 10:50:52 +0100

    bookinstancecontroller

--- express-library-manager/controllers/bookController.js
+++ express-library-manager/controllers/bookController.js
@@ -76,69 +76,72 @@ exports.book_create_get = asyncHandler(async (req, res, next) => {
 
 // Handle book create on POST.
 exports.book_create_post = [
+    (req, res, next)=>{
+      if (!(req.body.genre instanceof Array)){
+        if((typeof req.body.genre==="undefined"))
+          req.body.genre = []
+        else req.body.genre=[req.body.genre]
 
-  (req, res, next)=>{
-    if (!(req.body.genre instanceof Array)){
-      if((typeof req.body.genre==="undefined"))
-        req.body.genre = []
-      else req.body.genre=[req.body.genre]
-      console.log(req.body,"after")
-    }
-    next();
-  },
-
-  body('title', "title must not be empty")
-    .trim()
-    .isLength({min:1})
-    .escape(),
-  body('author', "Author must not be empty")
-    .trim()
-    .isLength({min:1})
-    .escape(),
-  body('summary', "summary must not be empty")
-    .trim()
-    .isLength({min:1})
-    .escape(),
-  body('isbn', "ISBN must not be empty")
-    .trim()
-    .isLength({min:1})
-    .escape(),
-  body('genre.*').escape(),
-   
-  asyncHandler(async (req, res, next) => {
-    const errors=validationResult(req);
-
-    const book=new Book({
-      title:req.body.title,
-      author:req.body.author,
-      summary:req.body.summary,
-      isbn:req.body.isbn,
-      genre:req.body.genre      
-    });
-
-    if(!errors.isEmpty){
-      const [authors, genres]=await Promise.all([
-        Author.find().exec(),
-        Genre.find().exec()
-      ]);
-
-      for (const genre of genres){
-        if (book.genre.includes(genre._id)){
-          genre.checked="true";
-        }
       }
+      next();
+    },
 
-      res.render("book_form", {
-        title:'Create book',
-        authors,
-        genres,
-        book,
-        errors:errors.array()
+    body('title')
+      .trim()
+      .isLength({min:1})
+      .escape()
+      .withMessage("title must be specified"),
+    body('author')
+      .trim()
+      .isLength({min:1})
+      .escape()
+      .withMessage("Author must be specified"),
+    body('summary')
+      .trim()
+      .isLength({min:1})
+      .escape()
+      .withMessage("summary must be specified"),
+    body('isbn')
+      .trim()
+      .isLength({min:1})
+      .escape()
+      .withMessage("ISBN must be specified"),
+    body('genre.*').escape(),
+
+    asyncHandler(async (req, res, next) => {
+      const errors=validationResult(req);
+
+      const book=new Book({
+        title:req.body.title,
+        author:req.body.author,
+        summary:req.body.summary,
+        isbn:req.body.isbn,
+        genre:req.body.genre      
       });
-    }else{
-      await book.save();
-      res.redirect(book.url);
-    }
+
+      if(!errors.isEmpty()){
+        const [authors, genres]=await Promise.all([
+          Author.find().exec(),
+          Genre.find().exec()
+        ]);
+
+        for (const genre of genres){
+          if (book.genre.includes(genre._id)){
+            genre.checked="true";
+          }
+        }
+
+        res.render("book_form", {
+          title:'Create book',
+          authors,
+          genres,
+          book,
+          errors:errors.array()
+        });
+      }else{
+        await book.save();
+        res.redirect(book.url);
+      }
 })
 ]
 
--- express-library-manager/controllers/bookinstanceController.js
+++ express-library-manager/controllers/bookinstanceController.js
@@ -4,6 +4,7 @@ const Genre=require('../models/genre');
 const Book=require('../models/book');
 
 const asyncHandler = require('express-async-handler');
+const {validationResult, body} = require('express-validator')
 
 
 exports.bookinstance_list=asyncHandler(async (req, res)=>{
@@ -26,13 +27,48 @@ exports.bookinstance_detail=asyncHandler(async (req, res, next)=>{
     });
 });
 
-exports.bookinstance_create_get=(req, res)=>{
-    res.send("not implement:bookinstance create get");
-};
+exports.bookinstance_create_get=asyncHandler(async (req, res, next)=>{
+    const books=await Book.find({}, "title").exec()
 
-exports.bookinstance_create_post=(req,res)=>{
-    res.send("not implemented: bookinstance create get");
-};
+    res.render("bookinstance_form",
+    {title:"Create bookinstance", books});
+});
+
+exports.bookinstance_create_post=[
+    body("book", "book must be specified")
+        .trim().isLength({min:1}).escape(),
+    body("imprint", "imprint must be specified")                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+        .trim().isLength({min:1}).escape(),
+    body("status").escape(),
+    body("due_back", "Invalid_date")
+        .optional({values:"falsy"})
+        .isISO8601()
+        .toDate(),
+
+    asyncHandler(async (req,res, next)=>{
+        const errors=validationResult(req);
+
+        const bookinstance=new BookInstance({
+            book:req.body.book,
+            imprint:req.body.imprint,
+            status:req.body.status,
+            due_back:req.body.due_back,
+        });
+
+        if (!errors.isEmpty()){
+            const books=await Book.find({}, "title").exec();
+
+            res.render("bookinstance_form",
+            {title:"Create Bookinstance",
+            books,
+            selected_book:bookInstance.book._id,
+            errors:errors.array(),
+            bookinstance:bookInstance
+    })
+        }
+    
+})
+]
 
 exports.bookinstance_delete_get=(req,res)=>{
     res.send("not implemented:bookinstance delete get");
